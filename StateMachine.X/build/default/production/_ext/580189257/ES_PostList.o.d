23aae8386b9ab6dfaefe0523f263479958b239e4
Aleksandr Kagan <alkagan@crm.ucsc.edu>
2017-11-20 15:32:59 -0800

    combined all template files so it compiles properly

---
+++ StateMachine.X/build/default/production/_ext/580189257/ES_PostList.o.d
@@ -0,0 +1,8 @@
+build/default/production/_ext/580189257/ES_PostList.o:  \
+ C:/Users/alkagan/Mechatronics_Final_Project/StateMachine.X/ES_PostList.c  \
+ C:/Users/alkagan/Mechatronics_Final_Project/StateMachine.X/ES_Configure.h  \
+ C:/Users/alkagan/Mechatronics_Final_Project/StateMachine.X/ES_General.h  \
+ C:/Users/alkagan/Mechatronics_Final_Project/StateMachine.X/ES_PostList.h  \
+ C:/Users/alkagan/Mechatronics_Final_Project/StateMachine.X/ES_Events.h  \
+ C:/Users/alkagan/Mechatronics_Final_Project/StateMachine.X/ES_ServiceHeaders.h  \
+ C:/Users/alkagan/Mechatronics_Final_Project/StateMachine.X/ES_KeyboardInput.h 
